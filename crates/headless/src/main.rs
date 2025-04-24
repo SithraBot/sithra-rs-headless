ca6386b93f19e2fbda7e6050398aaa732135202e
BERADQ <adqber123@outlook.com>
2025-04-24 23:31:31 +0800

    update: update

--- crates/headless/src/main.rs
+++ crates/headless/src/main.rs
@@ -52,7 +52,6 @@ impl SithraState for HeadlessState {
             "args": ["--headless","--no-sandbox",user_data_arg],
             "mobileEmulation": {
                 "deviceName": "iPhone 6/7/8",
-                "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1"
             },
             "excludeSwitches": ["disable-popup-blocking"],
         });
